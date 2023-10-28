948dd70ed0782773956d0a5930263226773b03a4
ubonisrael <akpanudohubonisrael@gmail.com>
2023-10-28 18:08:06 +0100

    added animations added form validation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const nav = document.querySelector("#nav");
 const openMenuBtn = document.querySelector("#open_nav");
 const closeMenuBtn = document.querySelector("#close_nav");
 const menuLinks = document.querySelectorAll(".menu_links");
-const projectsList = document.querySelector("#projects_list");
+const projectsList = document.querySelectorAll(".projects");
 
 const openNav = () => {
   nav.classList.remove("w-0");
@@ -24,11 +24,8 @@ menuLinks.forEach((menu_link) => {
 const loadProjects = () => {
   fetch("./data/projects.json")
     .then((res) => res.json())
-    .then((res) => {
-        const html =  res.map(data =>
-            (
-            `<li class="w-full flex flex-col justify-between p-4 sm:p-6 lg:p-8 rounded-md md:rounded-lg bg-white hover:bg-slate-300 drop-shadow sm:drop-shadow-md lg:drop-shadow-xl">
-        <div class="w-full">
+    .then((res) => res.map((data, i) => {
+            projectsList[i].innerHTML = `<div class="w-full">
         <img src="./data/images/${data.project_image}" alt={${data.name} image} />
         <h3 class="text-lg sm:text-xl md:text-2xl lg:text-3xl py-1 sm:py-4 lg:py-6 uppercase font-bold">
           ${data.name}
@@ -55,10 +52,49 @@ const loadProjects = () => {
             <iconify-icon icon="ph:link-bold"></iconify-icon>
             </a>
           </div>
-        </div>
-      </li>`)).join('')
-      projectsList.innerHTML = html
-    });
+        </div>`})
+    );
 };
 
-window.addEventListener("load", loadProjects);
+window.addEventListener("DOMContentLoaded", loadProjects);
+
+const regexp = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+function ValidationForm() {
+  const name = document.forms["contact_form"]["name"];
+  const email = document.forms["contact_form"]["email"];
+  const message = document.forms["contact_form"]["message"];
+  const nameError = document.querySelector(".name_error");
+  const emailError = document.querySelector(".email_error");
+  const messageError = document.querySelector(".message_error");
+  if (name.value == "") {
+    name.classList.replace('focus:border-slate-950', 'focus:border-red-600')
+    nameError.textContent = "Please enter your name."
+    name.focus();
+    return false;
+  }
+  if (email.value == "" || !regexp.test(email.value)) {
+    email.classList.replace('focus:border-slate-950', 'focus:border-red-600')
+    emailError.textContent = "Please enter a valid e-mail address."
+    email.focus();
+    return false;
+  }
+  if (message.value == "") {
+    message.classList.replace('focus:border-slate-950', 'focus:border-red-600')
+    messageError.textContent = "Please enter a message"
+    message.focus();
+    return false;
+  }
+  return true;
+}
+const formInputs = document.querySelectorAll('.form_input')
+formInputs.forEach(child => {
+    child.addEventListener('keypress', () => {
+        if (child.classList.contains('focus:border-red-600')) child.classList.replace('focus:border-red-600', 'focus:border-slate-950')
+    })
+})
+// const contact_form = document.forms["contact_form"]
+// contact_form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     return ValidationForm();
+// })
